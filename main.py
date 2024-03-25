423377d605acc34bf0e3c897f7b4a21530dc19b5
Wojtek_PC <wojciech05521@gmail.com>
2024-03-25 16:51:19 +0100

    basic net

--- main.py
+++ main.py
@@ -1,3 +1,112 @@
 import pygame
+import sys
+from typing import List
+from net import Net, Place
+from itertools import repeat
 
-print('hi')
+# IS_ANY_PLACE_SELECTED = False
+WINDOW_H = 1000
+WINDOW_W = 1000
+
+
+def draw_net(net: Net, surface: pygame.Surface, scalex = 1, scaley = 1):
+    for w in net.places:
+        pygame.draw.circle(surface, color="cornsilk2" if not w.color else w.color, center=((w.x * scalex, w.y * scaley)), radius=15)
+        for v in w.conns_map.values():
+            # for now each line is drawn 2 times
+            pygame.draw.line(surface, color="black", start_pos=((w.x * scalex, w.y * scaley)), end_pos=((v.x * scalex, v.y * scaley)), width=5)
+
+# def update_places(places: List[Place], surface: pygame.Surface):
+#     for place in places:
+#         if place.color:
+            
+
+def catch_cursor(places: List[Place]):
+    posx, posy = pygame.mouse.get_pos()
+    for place in places:
+        if abs(place.x - posx) < 10 and abs(place.y - posy) < 10:
+            if not place.color:
+                place.set_color("red")
+        else:
+            place.reset_color()
+
+def select_place(places: List[Place]):
+        # if IS_ANY_PLACE_SELECTED:
+        #     pass
+        posx, posy = pygame.mouse.get_pos()
+        for place in places:
+            if abs(place.x - posx) < 10 and abs(place.y - posy) < 10:
+                place.select()
+                place.set_color("blue")
+                show_neighbours(1, place)
+                return
+
+def reset_places(places: List[Place]):
+    for place in places:
+        place.unselect()
+        place.unset_show_accesible()
+
+def show_neighbours(n: int, place: Place):
+    for v in place.conns_map.values():
+        v.set_show_accesible()
+        v.set_color('pink')
+        if n - 1 > 0:
+            show_neighbours(n - 1, v)
+
+def reset_showed_neighbours(places: List[Place]):
+    for place in places:
+        place.set_show_accesible()
+
+# def select_next_place(place: Place):
+
+
+pygame.init()
+# infoObject = pygame.display.Info()
+# print(infoObject)
+screen = pygame.display.set_mode((WINDOW_W, WINDOW_H), pygame.RESIZABLE)
+pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
+
+# init surfaces
+# surf1 = pygame.Surface((1000, 900))
+# surf1.fill('azure4')
+surf2 = pygame.Surface((WINDOW_W, WINDOW_H))
+surf2.fill('azure4')
+
+net = Net(path='positions')
+net.make_conns('connections')
+# draw_net(net, surf2)
+
+while True:
+
+    draw_net(net, surf2, WINDOW_W / 1000, WINDOW_H / 1000)
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.VIDEORESIZE:
+            WINDOW_W, WINDOW_H = event.w, event.h
+            surf2 = pygame.Surface((WINDOW_W, WINDOW_H))
+            surf2.fill('azure4')
+            draw_net(net, surf2, WINDOW_W / 1000, WINDOW_H / 1000)
+        state = pygame.mouse.get_pressed()
+        # left click is being pressed
+        if state[0]:
+            select_place(net.places)
+        # right or middle click
+        if state[1] or state[2]:
+            reset_places(net.places)
+
+        # print(state)
+        catch_cursor(net.places)
+
+    # draw_net(net, surf2)    
+    
+    # screen.blit(surf1, (0, 0))
+    screen.blit(surf2, (0, 0))
+    # pygame.display.flip()
+
+    pygame.display.update()
+    clock.tick(60)        
---
+++ net.py
@@ -0,0 +1,87 @@
+import numpy as np
+from numpy.random import uniform
+from itertools import repeat
+import pytest
+from typing import List
+
+
+MAX_X = 500
+MAX_Y = 500
+
+
+class Place():
+    color = None
+    selected = False
+    show_accesible = False
+
+    def __init__(self, x, y, c) -> None:
+        self.c = c
+        self.x = x
+        self.y = y
+        self.connections = set()
+        self.conns_map :dict[int, Place] = {}
+    
+    def add_conn(self, n, obj):
+        self.connections.add(n)
+        self.conns_map[n] = obj
+    
+    def set_color(self, color):
+        self.color = color
+    
+    def reset_color(self):
+        if not any([self.selected, self.show_accesible]):
+            self.color = None
+    
+    def select(self):
+        self.selected = True
+    
+    def unselect(self):
+        self.selected = False
+
+    def set_show_accesible(self):
+        self.show_accesible =  not self.selected 
+    
+    def unset_show_accesible(self):
+        self.show_accesible =  False 
+    
+def read_poss(path, places: List[Place]) -> List[Place]:
+    counter = 0
+
+    with open(path) as f:
+        lines = f.readlines()
+        for line in lines:
+            elems = line.split(' ')
+            places.append(Place(float(elems[0]), float(elems[1]), counter))
+            counter += 1
+    return places
+
+def read_cons(path, places: List[Place]) -> None:
+
+    with open(path) as f:
+        lines = f.readlines()
+        for line in lines:
+            elems = line.split(' ')
+            v, w = int(elems[0]), int(elems[1])
+            places[v].add_conn(w, places[w])
+            places[w].add_conn(v, places[v])
+
+class Net():
+    places = []
+
+    def __init__(self, n=0, random = False, path=None) -> None:
+        if path:
+            read_poss(path, self.places)
+        elif random:
+            self.places.extend(repeat(Place(uniform(high=MAX_X), uniform(high=MAX_Y)), n))
+        else:
+            self.places.extend(repeat(Place(0, 0), n))
+    
+    def make_conns(self, path):
+        read_cons(path, self.places)
+
+
+
+# for testing purposes
+if __name__ == '__main__':
+    net = Net(path='positions')
+    pytest.set_trace()
